29dcbd96c9b64471c086f5b7bf21a4eaf20120d4
PaulYu1988 <52305929+PaulYu1988@users.noreply.github.com>
2025-04-16 14:22:22 +0800

    Update SessionExtensions.cs

--- YBlog/Extensions/SessionExtensions.cs
+++ YBlog/Extensions/SessionExtensions.cs
@@ -14,5 +14,19 @@ namespace YBlog.Extensions
             var value = session.GetString(key);
             return value == null ? default : JsonConvert.DeserializeObject<T>(value);
         }
+
+        public static void SetBool(this ISession session, string key, bool value)
+        {
+            session.SetInt32(key, value ? 1 : 0);
+        }
+        public static bool? GetBool(this ISession session, string key)
+        {
+            var value = session.GetInt32(key);
+            if (value == null)
+            {
+                return null;
+            }
+            return value == 1;
+        }
     }
 }
